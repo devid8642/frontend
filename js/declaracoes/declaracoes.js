48a0f4f27b324960b0c976b3eb86cfc1fdc1fa8f
devid <nnick4673@gmail.com>
2022-12-05 10:09:03 -0300

    O básico sobre declarações

---
+++ js/declaracoes/declaracoes.js
@@ -0,0 +1,21 @@
+// Declarando e definindo valores de variáveis
+let a;
+var b, c;
+a = 7;
+b = 8;
+c = 7 + 8;
+console.log(c);
+
+// JS suporta os principais operadores matemáticos
+
+console.log(4 + 4);
+console.log(8 - 9);
+console.log(23 * 56);
+console.log(15 / 15);
+
+// Para trabalhor com string no JS usamos aspas duplas ou simples
+
+var nome = "Devid";
+console.log(nome + " Teofilo");
+
+// Recomenda-se finalizar toda declaração JS com ;
