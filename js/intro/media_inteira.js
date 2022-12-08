0e8c6dcc80f76276f7524b7ea118c62eae3f123b
devid <nnick4673@gmail.com>
2022-12-08 18:02:45 -0300

    Solução de um problema em JS

---
+++ js/intro/media_inteira.js
@@ -0,0 +1,7 @@
+// Link do problema: https://neps.academy/br/exercise/136
+const rd = require("readline-sync");
+
+let a = Number(rd.question());
+let b = Number(rd.question());
+
+console.log(Math.floor((a + b)/2));
