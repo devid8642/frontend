e2ba0d44c5c362b08b99fd99b48c0b4b85bf6bcf
devid <nnick4673@gmail.com>
2022-12-08 18:32:50 -0300

    Solução de problema em JS

---
+++ js/intro/repetir.js
@@ -0,0 +1,8 @@
+// Link do problema: https://neps.academy/br/exercise/157
+const rd = require("readline-sync");
+
+let num = Number(rd.question());
+
+for (let i = 0; i < num; i++) {
+	console.log("NepsAcademy eh Sucesso");
+}
