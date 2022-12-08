88092dfe05908209183c690459593e9fa3b828fc
devid <nnick4673@gmail.com>
2022-12-08 17:53:17 -0300

    Solução problema soma fácil

---
+++ js/intro/soma_facil.js
@@ -0,0 +1,6 @@
+// link do problema: https://neps.academy/br/exercise/134
+const rd = require("readline-sync");
+
+let a = Number(rd.question());
+let b = Number(rd.question());
+console.log(a + b);
