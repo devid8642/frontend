7629ae91e89d6869eb82136e978f18a18da35f21
devid <nnick4673@gmail.com>
2023-04-27 12:00:17 -0300

    Comentários em JS

---
+++ rocketseat/guia_de_js/comentarios.js
@@ -0,0 +1,7 @@
+// Os comentários podem ser indicados com duas barras
+/*
+    Ou usando barra e asterísco. a diferença é que aqui podemos ter comentários com múltiplas linhas.
+*/
+// Comentários geralmente servem para ajudar na compreensão do código
+// O código abaixo imprime Olá mundo! no console.
+console.log("Olá Mundo!");
