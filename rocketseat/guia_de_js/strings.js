89367e40a56ebf32dd0d046332a4f17073a56b78
devid <nnick4673@gmail.com>
2023-04-27 12:05:08 -0300

    Strings em JS

---
+++ rocketseat/guia_de_js/strings.js
@@ -0,0 +1,10 @@
+// Strings são cadeias de caracteres, ou seja, textos de uma forma em geral.
+
+// é possível representa-las usando aspas simples
+console.log('String com aspas simples');
+
+// Com aspas duplas
+console.log("String com aspas duplas");
+
+// E com as crases, usando templates strings
+console.log(`1 + 1 = ${1 + 1}`);
